84e5596978450abacad51a9050247bdc91f00771
Kevin Martensson <kevinmartensson@gmail.com>
2015-04-20 20:59:15 +0200

    Use `mocha` for testing

--- test/test.js
+++ test/test.js
@@ -1,42 +1,46 @@
+/*global afterEach,beforeEach,it*/
 'use strict';
 
+var assert = require('assert');
 var execFile = require('child_process').execFile;
 var fs = require('fs');
 var path = require('path');
 var binCheck = require('bin-check');
 var BinBuild = require('bin-build');
 var compareSize = require('compare-size');
-var test = require('ava');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
 var tmp = path.join(__dirname, 'tmp');
 
-test('rebuild the zopflipng binaries', function (t) {
-	t.plan(2);
+beforeEach(function () {
+	mkdirp.sync(tmp);
+});
 
+afterEach(function () {
+	rimraf.sync(tmp);
+});
+
+it('rebuild the zopflipng binaries', function (cb) {
 	new BinBuild()
 		.src('https://github.com/google/zopfli/archive/bce73e2c23dc57a252802e4a6df97aa675fcea81.zip')
 		.cmd('mkdir -p ' + tmp)
 		.cmd('make zopflipng && mv ./zopflipng ' + path.join(tmp, 'zopflipng'))
 		.run(function (err) {
-			t.assert(!err, err);
-
-			fs.exists(path.join(tmp, 'zopflipng'), function (exists) {
-				t.assert(exists);
-			});
+			assert(!err);
+			assert(fs.statSync(path.join(tmp, 'zopflipng')).isFile());
+			cb();
 		});
 });
 
-test('return path to binary and verify that it is working', function (t) {
-	t.plan(2);
-
+it('return path to binary and verify that it is working', function (cb) {
 	binCheck(require('../').path, ['--help'], function (err, works) {
-		t.assert(!err, err);
-		t.assert(works);
+		assert(!err);
+		assert(works);
+		cb();
 	});
 });
 
-test('minify a PNG', function (t) {
-	t.plan(3);
-
+it('minify a PNG', function (cb) {
 	var src = path.join(__dirname, 'fixtures/test.png');
 	var dest = path.join(tmp, 'test.png');
 	var args = [
@@ -46,11 +50,12 @@ test('minify a PNG', function (t) {
 	];
 
 	execFile(require('../').path, args, function (err) {
-		t.assert(!err, err);
+		assert(!err);
 
 		compareSize(src, dest, function (err, res) {
-			t.assert(!err, err);
-			t.assert(res[dest] < res[src]);
+			assert(!err);
+			assert(res[dest] < res[src]);
+			cb();
 		});
 	});
 });
