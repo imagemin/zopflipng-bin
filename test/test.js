bcb09f7b4a4c18a6a1e825940552c2f912adbb51
1000ch <shogosensui@gmail.com>
2022-05-07 23:32:48 +0900

    Update dependencies

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ import path from 'node:path';
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
-import execa from 'execa';
-import tempy from 'tempy';
+import {execa} from 'execa';
+import {temporaryDirectory} from 'tempy';
 import binCheck from 'bin-check';
 import binBuild from 'bin-build';
 import compareSize from 'compare-size';
@@ -17,7 +17,7 @@ test('rebuild the zopflipng binaries', async t => {
 		return;
 	}
 
-	const temporary = tempy.directory();
+	const temporary = temporaryDirectory();
 	const source = fileURLToPath(new URL('../vendor/source/zopfli-1.0.3.tar.gz', import.meta.url));
 
 	await binBuild.file(source, [
@@ -33,7 +33,7 @@ test('return path to binary and verify that it is working', async t => {
 });
 
 test('minify a PNG', async t => {
-	const temporary = tempy.directory();
+	const temporary = temporaryDirectory();
 	const src = fileURLToPath(new URL('fixtures/test.png', import.meta.url));
 	const dest = path.join(temporary, 'test.png');
 	const args = [
